4264bdaf22a0f6897cda11410f5656f08adf6161
Rasmus Sild <rasmus.sild@gmail.com>
2026-06-22 19:46:11 +0300

    Initial commit

---
+++ backend/src/main/java/com/sild/securechat_backend/SecurechatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sild.securechat_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurechatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurechatBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sild/securechat_backend/SecurechatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sild.securechat_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurechatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
